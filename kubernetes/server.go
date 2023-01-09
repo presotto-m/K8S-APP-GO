9ff288c1be112b631c8b40880c1fe65715be6951
presotto-m <matheuspresotto@icloud.com>
2023-01-09 14:07:24 -0300

    Cluster K8S APP em GO

---
+++ kubernetes/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+
+	name := os.Getenv("NAME	")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
